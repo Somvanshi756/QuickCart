c7cf8ad73cc91f49ea4139fe3ab004f0260496f5
Somvanshi756 <rishisomvanshi756@gmail.com>
2025-09-04 02:37:32 +0530

    Update inngest.js

--- config/inngest.js
+++ config/inngest.js
@@ -27,7 +27,7 @@ export const syncUserCreation = inngest.createFunction(
 
 
 // inngest functions to update user data in a database
-export const syncUserUpdate = inngest.createFunction(
+export const syncUserUpdation = inngest.createFunction(
     { 
          id: 'update-user-from-clerk'
     },
@@ -35,6 +35,7 @@ export const syncUserUpdate = inngest.createFunction(
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
+            _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
@@ -46,7 +47,7 @@ export const syncUserUpdate = inngest.createFunction(
 
 
 // inngest functions to delete user data in a database
-export const syncUserDelete = inngest.createFunction(
+export const syncUserDeletion = inngest.createFunction(
     { 
          id: 'delete-user-with-clerk'
     },
